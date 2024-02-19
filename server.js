041ae0b10a566d91f9f333ced2ae28899c7a5785
Adriano-henriqq <henrique3197@hotmail.com>
2024-02-19 10:45:03 -0300

    config server js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app = express();
 app.use('/', express.static(resolve(__dirname,'./build')
     )
 )
+app.use('/Contatos', express.static(resolve(__dirname,'./build')
+    )
+)
+app.use('/Resumo', express.static(resolve(__dirname,'./build')
+    )
+)
 app.listen(process.env.PORT || 3000, (err)=>{
     if(err) {return console.log(err)}
     console.log('tudo ok')
